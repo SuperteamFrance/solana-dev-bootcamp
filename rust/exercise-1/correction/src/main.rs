7495831684c3706c6b410dbcd4f66e1d4841747a
Odomart <odomart69@gmail.com>
2025-01-16 19:12:23 +0100

    correction exo1

---
+++ rust/exercise-1/correction/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // Déclarer ici la variable age
+    let age: u8 = 25;
+
+    // Déclarer ici la constante MAX_SCORE
+    const MAX_SCORE: u32 = 100;
+
+    // Déclarer ici la variable temperature
+    let temperature: i32 = -5;
+
+    // Afficher toutes les valeurs
+    println!("Votre âge : {}", age);
+    println!("Score maximum : {}", MAX_SCORE);
+    println!("Température : {}", temperature);
+}
