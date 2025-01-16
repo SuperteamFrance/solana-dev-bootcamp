d28abfd690eb8a0d9020c1e047073d1f1dd8e555
Odomart <odomart69@gmail.com>
2025-01-16 18:21:43 +0100

    refacto: remove main3

--- rust/exercise-6/main3.rs
+++
@@ -1,9 +0,0 @@
-fn obtenir_longueur(s: &String) -> usize {
-    s.len()
-}
-
-fn main() {
-    let mut s = String::from("Hello");
-    let r1 = &mut s;
-    println!("Longueur: {}", obtenir_longueur(&s)); // Erreur ici
-}
